c9db3e7ccf56e3f8a943739bdcfea68c32e0a6ac
charltonaustin <charlton.austin@gmail.com>
2025-02-15 18:49:37 -0800

    Adding in a form for accounts

--- src/Handler/Accounts.hs
+++ src/Handler/Accounts.hs
@@ -1,49 +1,115 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Handler.Accounts where
-import           Data.Text                (Text, pack)
-import Foundation ( DB, Handler )
+
+import Control.Monad.IO.Class (liftIO)
+import Data.Text (Text, pack)
+import Data.Time (Day, getCurrentTime, utctDay)
+import Foundation (DB, Form, Handler, Route (AccountsR))
 import Import.NoFoundation
-    ( ($),
-      Semigroup((<>)),
-      widgetFile,
-      Comment,
-      Entity,
-      (.),
-      toHtml,
-      setTitle,
-      selectList,
-      Html,
-      Entity(entityVal),
-      SelectOpt(Asc),
-      EntityField(CommentId),
-      Comment(commentMessage),
-      Yesod(defaultLayout),
-      YesodPersist(runDB) )
+  ( Applicative ((<*>)),
+    Asset (..),
+    AssetAccount (..),
+    AssetDeposit (..),
+    AssetValue (..),
+    Comment (commentMessage),
+    Entity (entityVal),
+    EntityField (AssetAccountId),
+    FieldSettings
+      ( FieldSettings,
+        fsAttrs,
+        fsId,
+        fsLabel,
+        fsName,
+        fsTooltip
+      ),
+    Html,
+    IO,
+    Maybe (..),
+    SelectOpt (Asc),
+    Semigroup ((<>)),
+    Yesod (defaultLayout),
+    YesodPersist (runDB),
+    aopt,
+    entityKey,
+    fmap,
+    generateFormPost,
+    insertEntity,
+    selectList,
+    setTitle,
+    textField,
+    toHtml,
+    widgetFile,
+    ($),
+    (.),
+    (<$>),
+  )
+import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 
 getAccountsR :: Handler Html
 getAccountsR = do
-  allComments <- runDB $ getAllComments
+  (_, formEnctype) <- generateFormPost sampleForm
+  allAccounts <- runDB $ getAllAccounts
 
   defaultLayout $ do
     let (_, _, commentListId) = commentIds
-    setTitle . toHtml $ (pack "Charlton")  <> "'s User page"
+    setTitle . toHtml $ (pack "Charlton") <> "'s User page"
     $(widgetFile "accounts")
 
 deleteAccountsR :: Handler Html
 deleteAccountsR = do
-  allComments <- runDB $ getAllComments
+  (_, formEnctype) <- generateFormPost sampleForm
+  allAccounts <- runDB $ getAllAccounts
 
   defaultLayout $ do
     let (_, _, commentListId) = commentIds
-    setTitle . toHtml $ (pack "Charlton")  <> "'s User page"
+    setTitle . toHtml $ (pack "Charlton") <> "'s User page"
     $(widgetFile "accounts")
 
-getAllComments :: DB [Entity Comment]
-getAllComments = selectList [] [Asc CommentId]
+postAccountsR :: Handler Html
+postAccountsR = do
+  (_, formEnctype) <- generateFormPost sampleForm
+  today <- liftIO getToday
+  accountEntity <- runDB $ insertEntity $ AssetAccount "Fidelity Individual" "...555"
+  let accountId = entityKey accountEntity -- Extract primary key
+  _ <- runDB $ insertEntity $ AssetDeposit 50000 accountId today
+  allAccounts <- runDB $ getAllAccounts
+
+  defaultLayout $ do
+    let (_, _, commentListId) = commentIds
+    setTitle . toHtml $ (pack "Charlton") <> "'s User page"
+    $(widgetFile "accounts")
+
+getAllAccounts :: DB [Entity AssetAccount]
+getAllAccounts = selectList [] [Asc AssetAccountId]
 
 commentIds :: (Text, Text, Text)
 commentIds = ("js-commentForm", "js-createCommentTextarea", "js-commentList")
+
+getToday :: IO Day
+getToday = fmap utctDay getCurrentTime
+
+data AccountForm = AccountForm
+  { name :: Maybe Text
+  }
+
+sampleForm :: Form AccountForm
+sampleForm =
+  renderBootstrap3 BootstrapBasicForm $ AccountForm <$> aopt textField textSettings Nothing
+  where
+    -- Add attributes like the placeholder and CSS classes.
+    textSettings =
+      FieldSettings
+        { fsLabel = "Name",
+          fsTooltip = Nothing,
+          fsId = Nothing,
+          fsName = Nothing,
+          fsAttrs =
+            [ ("class", "form-control"),
+              ("placeholder", "Name")
+            ]
+        }
